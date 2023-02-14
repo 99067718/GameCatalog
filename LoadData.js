68956c1eaea38c5a3a4805cb99df03a72b8c8dac
Mielesgames <emieltlam11@gmail.com>
2023-02-14 13:30:36 +0100

    yes

--- LoadData.js
+++ LoadData.js
@@ -7,34 +7,61 @@ var dict = {
           "Image":"https://tr.rbxcdn.com/c0498ada296728d8af442614f53ccd16/768/432/Image/Png",
           "ShortDescription":"Doors is a Roblox horror",
           "Rating": 4,
-          "Genre":"Horror"
+          "Genre":"Horror",
+          "Icon": "https://static.wikia.nocookie.net/doors-game/images/a/aa/DOORS_Icon.png/revision/latest?cb=20220919210414",
+          "Tag": "Horror, First Person"
       },
       {
           "Name":"Minecraft",
           "Link":"https://www.minecraft.net/en-us",
           "Description":"Minecraft is a block game with blocks",
           "Image":"https://fs-prod-cdn.nintendo-europe.com/media/images/10_share_images/games_15/nintendo_switch_4/H2x1_NSwitch_Minecraft.jpg",
-          "ShortDescription":"Doors is a Roblox horror",
+          "ShortDescription":"Minecraft is a game with blocks",
           "Rating": 5,
-          "Genre":"Horror"
+          "Genre":"Horror",
+          "Icon": "https://cdn.icon-icons.com/icons2/2699/PNG/512/minecraft_logo_icon_168974.png"
       },
       {
-        "Name":"test",
-        "Link":"https://www.minecraft.net/en-us",
-        "Description":"Minecraft is a block game with blocks",
-        "Image":"https://fs-prod-cdn.nintendo-europe.com/media/images/10_share_images/games_15/nintendo_switch_4/H2x1_NSwitch_Minecraft.jpg",
-        "ShortDescription":"Doors is a Roblox horror",
+        "Name":"Rick's Relentless Rhythm",
+        "Link":"https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+        "Description":"He will never give you up and never let you down.",
+        "Image":"https://i.insider.com/602ee9e3d3ad27001837f2af?width=1200&format=jpeg",
+        "ShortDescription":"Never gonna give you...",
         "Rating": 5,
-        "Genre":"Horror"
+        "Genre":"Giving Up",
+        "Icon": "https://media.tenor.com/x8v1oNUOmg4AAAAd/rickroll-roll.gif"
     }
   ]
           
 };
 console.log(dict['Games'].length)
 console.log(document.getElementById("GameName"));
-document.getElementById('GameName').innerHTML = "monke";
 
-for (i = 1; i < dict['Games'].length; i ++){
-  let items = document.querySelector('.TemplateSmall').cloneNode(true);
-  document.body.appendChild(items)
+var newName = dict['Games'][0].Name;
+console.log(newName)
+
+for (i = 0; i < dict['Games'].length; i ++){
+  console.log('Created')
+  var element = document.createElement("article");
+  var gameTitle = document.createElement("h1");
+  var gameDescription = document.createElement('p');
+  var gameIcon = document.createElement("img");
+  var IconAndName = document.createElement("div");
+  
+  gameDescription.innerText = dict['Games'][i].ShortDescription;
+  gameTitle.innerText = dict['Games'][i].Name;
+  gameIcon.src = dict['Games'][i].Icon;
+  gameIcon.width = 150;
+  gameIcon.height = 150;
+  gameIcon.classList.add("Icons");
+  IconAndName.classList.add("MainItems")
+  
+  IconAndName.appendChild(gameTitle);
+  IconAndName.appendChild(gameIcon);
+  element.appendChild(IconAndName);
+  element.appendChild(gameDescription);
+  element.classList.add("TemplateSmall");
+
+  var mainDiv = document.getElementById("TemplateMain");
+  mainDiv.appendChild(element);
 }
