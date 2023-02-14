b13b7b7536bb916f5858f3302b7db76a64b86938
Mielesgames <emieltlam11@gmail.com>
2023-02-14 09:58:16 +0100

    test

--- LoadData.js
+++ LoadData.js
@@ -1,7 +1,20 @@
-fetch("./data.json")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  })
+var dict = {
+  "Games":[
+      {
+          "Name":"Doors",
+          "Link":"https://www.roblox.com/games/6516141723",
+          "Description":"Doors is a Roblox horror game where you enter a hotel with 100 doors, in every room you can find items \n (for example: coins, flashlight, candle) there are also a bunch of monsters that try to stop you from leaving the hotel, can you leave the hotel before they catch you?",
+          "Image":"https://tr.rbxcdn.com/c0498ada296728d8af442614f53ccd16/768/432/Image/Png",
+          "ShortDescription":"Doors is a Roblox horror"
+      },
+      {
+          "Name":"Minecraft",
+          "Link":"https://www.roblox.com/games/6516141723",
+          "Description":"Minecraft is a block game with blocks",
+          "Image":"https://tr.rbxcdn.com/c0498ada296728d8af442614f53ccd16/768/432/Image/Png",
+          "ShortDescription":"Doors is a Roblox horror"
+      }
+  ]
+          
+}
+document.getElementById("name").innerHTML = "monke";
